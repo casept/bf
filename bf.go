20b936d087981ab59125fae81610d88ff54bbb4d
casept <davids.paskevics@gmail.com>
2018-01-17 21:30:02 +0100

    Some more work

--- bf.go
+++ bf.go
@@ -10,71 +10,120 @@ import (
 func main() {
 	// The tape has as many cells as can be adressed by an int32
 	var tape [2147483648]uint32
-	var pointer uint32
-	var bracketCount int
+	var iPointer int
 	// Read in the program
 	file := os.Args[1]
-	programB, err := ioutil.ReadAll(file)
-	program := rune(string(programB))
+	fDesc, err := os.Open(file)
+	if err != nil {
+		fmt.Printf("Error while reading file: %v\n", err)
+		os.Exit(1)
+	}
+	defer fDesc.Close()
+	programB, err := ioutil.ReadAll(fDesc)
+	program := []rune(string(programB))
 	if err != nil {
 		panic(err)
 	}
+	// Get maps matching opening and closing brackets
+	openCloseMap, closeOpenMap := getBrackPairs(program)
 
 	// Create a bufio reader for stdin
 	rd := bufio.NewReader(os.Stdin)
 	// Just serially read each instruction
-	for i, token := range program {
+	dPointer := 0
+	for iPointer < len(program) {
+		token := program[iPointer]
 		switch token {
 		// TODO: prevent overflow
-		case "+":
-			tape[pointer] = tape[pointer] + 1
-		case "-":
-			tape[pointer] = tape[pointer] - 1
-		case ">":
-			pointer++
-		case "<":
-			pointer--
-		case ".":
-			fmt.Printf(tape[pointer])
-		case ",":
-			rd.ReadString('\n')
-			// TODO
-			// TODO: Validate that there are no loose "]" without a match
-		case "[":
-			// Keep track of the number of brackets passed so we can match.
-			bracketCount++
-			if tape[pointer] == 0 {
-				// Read all tokens until the matching "]" is found
-
+		case '+':
+			tape[dPointer] = tape[dPointer] + 1
+			iPointer++
+		case '-':
+			tape[dPointer] = tape[dPointer] - 1
+			iPointer++
+		case '>':
+			dPointer++
+			iPointer++
+		case '<':
+			dPointer--
+			iPointer++
+		case '.':
+			fmt.Printf("%v", string(rune(tape[dPointer])))
+			iPointer++
+		case ',':
+			inStr, err := rd.ReadString('\n')
+			if err != nil {
+				fmt.Printf("Failed to read from stdin: %v\n", err)
+				os.Exit(1)
 			}
-		case "]":
-			// TODO
+			inRuneSlice := []rune(inStr)
+			if len(inRuneSlice) != 1 {
+				fmt.Printf("You can only input 1 character at a time!")
+			} else {
+				inRune := inRuneSlice[0]
+				tape[dPointer] = uint32(inRune)
+				if err != nil {
+					fmt.Printf("Failed to read from stdin: %v\n", err)
+					os.Exit(1)
+				}
+			}
+			iPointer++
+		case '[':
+			//
+			if tape[dPointer] == 0 {
+				// Look up what "]" to jump to
+				iPointer = openCloseMap[iPointer] + 1
+
+			} else {
+				iPointer++
+			}
+		case ']':
+			if tape[dPointer] != 0 {
+				// Look up what "[" to jump to
+				iPointer = closeOpenMap[iPointer] + 1
+
+			} else {
+				iPointer++
+			}
 		default:
-			// Invalid token, do nothing
+			// Unknown token, do nothing
 		}
 	}
 }
 
-func getBrackPairs(program string) (brackPairs map[int]int) {
-	// search for "["
-	var leftBracks []int
-	var rightBracks []int
+// Parse the program and return a map with [index_of_opening_bracket]index_of_closing_bracket
+func getBrackPairs(program []rune) (map[int]int, map[int]int) {
+	var openBracks []int
+	var closeBracks []int
 	// TODO: Paralellize
+	// search for "[" from the beginning
 	for i, token := range program {
-		if token == "[" {
-			append(leftBracks, i)
+		if token == '[' {
+			openBracks = append(openBracks, i)
 		}
 
 	}
-	// Now search for "]"
+	// Now search for "]" from the end
 	for i := len(program) - 1; i >= 0; i++ {
-		if program[i] == "]" {
-			append(rightBracks, i)
+		if program[i] == ']' {
+			closeBracks = append(closeBracks, i)
 		}
 	}
 
-	// Now glue them together into a map
-	for i := 0; i < len(leftBracks); i++ {
-
+	if len(openBracks) != len(closeBracks) {
+		// TODO: Print index
+		fmt.Print("Missing opening or closing bracket!")
+		os.Exit(1)
 	}
+	openCloseMap := make(map[int]int)
+	closeOpenMap := make(map[int]int)
+	// Now glue them together into a map
+	for i := 0; i < len(openBracks); i++ {
+		openCloseMap[openBracks[i]] = closeBracks[i]
+	}
+	// And again in reverse, as go doesn't allow looking up a key by value.
+	for i := 0; i < len(closeBracks); i++ {
+		closeOpenMap[closeBracks[i]] = openBracks[i]
+	}
+	return openCloseMap, closeOpenMap
 }
